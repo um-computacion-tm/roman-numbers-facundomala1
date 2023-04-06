3d87bba8f52bd37b630a5c9b7d4fba57fba1c9d6
facundomala1 <f.mala@alummno.um.edu.ar>
2023-04-06 12:35:53 -0300

    separacion de test

--- codigoNumROM.py
+++ codigoNumROM.py
@@ -1,61 +1,45 @@
-import unittest
+
 
 def decimal_to_roman(decimal):
-    if decimal <= 3:
-        return "I" * decimal
-    elif decimal ==5:
-        return "V"
-    elif decimal ==15:
-        return "XV"
-    elif decimal ==20:
-        return "XX"
-    elif decimal ==50:
-        return "L"
-    else:
-        return "X"
+    roman = ""
+    while decimal >= 1000:
+        roman += "M"
+        decimal -= 1000
+    if decimal >= 900:
+        roman += "CM"
+        decimal -= 900
+    elif decimal >= 500:
+        roman += "D"
+        decimal -= 500
+    elif decimal >= 400:
+        roman += "CD"
+        decimal -= 400
+    while decimal >= 100:
+        roman += "C"
+        decimal -= 100
+    if decimal >= 90:
+        roman += "XC"
+        decimal -= 90
+    elif decimal >= 50:
+        roman += "L"
+        decimal -= 50
+    elif decimal >= 40:
+        roman += "XL"
+        decimal -= 40
+    while decimal >= 10:
+        roman += "X"
+        decimal -= 10
+    if decimal >= 9:
+        roman += "IX"
+        decimal -= 9
+    elif decimal >= 5:
+        roman += "V"
+        decimal -= 5
+    elif decimal >= 4:
+        roman += "IV"
+        decimal -= 4
+    while decimal > 0:
+        roman += "I"
+        decimal -= 1
+    return roman
 
-
-
-class testDecimalRomano(unittest.TestCase):
-    def test_1(self):
-        #pre condicion
-        ##No HAY
-
-        #PROCESO
-        resultado = decimal_to_roman(1)
-
-        #verificado
-
-        self.assertEqual(resultado, "I")
-
-    def test_diez(self):
-        resultado = decimal_to_roman(10)
-        self.assertEqual(resultado, "X")    
-
-
-    def test_cinco(self):
-        resultado=decimal_to_roman(5)
-        self.assertEqual(resultado, "V")
-
-    def test_dos(self):
-        resultado=decimal_to_roman(2)
-        self.assertEqual(resultado, "II")
-
-    def test_tres(self):
-        resultado=decimal_to_roman(3)
-        self.assertEqual(resultado, "III")
-
-    def test_quince(self):
-        resultado=decimal_to_roman(15)
-        self.assertEqual(resultado,"XV")
-
-    def test_veinte(self):
-        resultado=decimal_to_roman(20)
-        self.assertEqual(resultado,"XX")
-
-    def test_cincuenta(self):
-        resultado=decimal_to_roman(50)
-        self.assertEqual(resultado,"L")    
-
-if __name__ ==  "__main__":
-    unittest.main() 
